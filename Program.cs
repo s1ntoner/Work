76bab77d35a192e8da7dfb2edf6a385330acb5c5
s1ntoner <snroner229@gmail.com>
2025-09-08 15:53:16 +0200

    karta pracy 1/2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,35 @@
-﻿////zad 1
+﻿//zad 1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a*a + b*b);
+//zad2
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine((a + b) * (a + b));
+//zad3
+//int a = int.Parse(Console.ReadLine()); 
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine((a + b)^3);
+//zad4
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//Console.WriteLine(a * b * c);
+//zad5
+//float a = float.Parse(Console.ReadLine());
+//float b = float.Parse(Console.ReadLine());
+//Console.WriteLine(2 * (a + b) / 5);
+//Console.WriteLine((a + b) * 0.4);
+//zad6
+//float a = float.Parse(Console.ReadLine());
+//Console.WriteLine(a / 1.23);
+//zad7
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a%b);
+
+//karta pracy 2
+////zad 1
 //int a = int.Parse(Console.ReadLine());
 //if (a % 3 == 0)
 //    Console.WriteLine("TAK");
