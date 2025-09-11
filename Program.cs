426f12ac7f9769fabad0a4542666784b9ff4c776
s1ntoner <snroner229@gmail.com>
2025-09-11 15:12:40 +0200

    Karta pracy 2 and 2 a

--- Program.cs
+++ Program.cs
@@ -1,43 +1,52 @@
-﻿//zad 1
+﻿//zadanie3
 //int a = int.Parse(Console.ReadLine());
-//int b = int.Parse(Console.ReadLine());
-//Console.WriteLine(a*a + b*b);
-//zad2
+//if (a < 18)
+//    Console.WriteLine("Jesteś pełnoletni");
+//else
+//    Console.WriteLine("Jesteś nie pełnoletni");
+
+
+//zadanie4
 //int a = int.Parse(Console.ReadLine());
-//int b = int.Parse(Console.ReadLine());
-//Console.WriteLine((a + b) * (a + b));
-//zad3
-//int a = int.Parse(Console.ReadLine()); 
-//int b = int.Parse(Console.ReadLine());
-//Console.WriteLine((a + b)^3);
-//zad4
+//if (a < 20)
+//    Console.WriteLine("TAK");
+//else
+//    Console.WriteLine("NIE");
+
+//zadanie5
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
 //int c = int.Parse(Console.ReadLine());
-//Console.WriteLine(a * b * c);
-//zad5
-//float a = float.Parse(Console.ReadLine());
-//float b = float.Parse(Console.ReadLine());
-//Console.WriteLine(2 * (a + b) / 5);
-//Console.WriteLine((a + b) * 0.4);
-//zad6
-//float a = float.Parse(Console.ReadLine());
-//Console.WriteLine(a / 1.23);
-//zad7
+//if ((c > a && c < b) || (c > b && c < a))
+//    Console.WriteLine("TAK");
+//else 
+//    Console.WriteLine("NIE");
+
+//zadanie6
+//int a = int.Parse(Console.ReadLine());
+//int p = int.Parse(Console.ReadLine());
+//if (Math.Pow(a, p) - a % p == 0)
+//    Console.WriteLine("Tak");
+//else
+//    Console.WriteLine("NIE");
+
+
+
+//Karta pracy 2a 
+//zadanie1
 //int a = int.Parse(Console.ReadLine());
 //int b = int.Parse(Console.ReadLine());
-//Console.WriteLine(a%b);
+//int c = a + b;
+//if (c % 2 == 0)
+//    Console.WriteLine("TAK");
+//else Console.WriteLine("NIE");
 
-//karta pracy 2
-////zad 1
+//zadanie2
 //int a = int.Parse(Console.ReadLine());
-//if (a % 3 == 0)
+//int b = int.Parse(Console.ReadLine());
+//if ((a+b)/2 > Math.Sqrt(a*b))
 //    Console.WriteLine("TAK");
-//else
-//    Console.WriteLine("NIE");
-//zad2
-//int a = int.Parse(Console.ReadLine());
-//if (a >= 100 || a < 999 && a % 17 == 00)
-//    Console.WriteLine("TAK");
-//else
-//    Console.WriteLine("NIE");
+//else Console.WriteLine("NIE");
+
+
+//zadanie3
